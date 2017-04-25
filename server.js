ea35f96cd4d04449cce851c15efdaa8744e41eac
maade7 <martin.ade@gatech.edu>
2017-04-25 17:22:14 -0400

    fix on heroku

--- server.js
+++ server.js
@@ -6,30 +6,32 @@ var express = require("express"),
     mysql = require("mysql"),
     path = require("path");
 
-var PORT = process.env.PORT || 8080;
+// var PORT = process.env.PORT || 8080;
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    port     : 3300,
-    user     : 'root',
-    password : 'maade',
-    database : 'friendFinder'
-});
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     port     : 3300,
+//     user     : 'root',
+//     password : 'maade',
+//     database : 'friendFinder'
+// });
 
 
-connection.connect();
-
-connection.query("", function (req, res) {
-
-});
+// connection.connect();
+//
+// connection.query("", function (req, res) {
+//
+// });
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-// app.use(express.static('app'));
+
+app.use(express.static(__dirname + '/app'));
 
 // Routes
 // =============================================================
@@ -38,6 +40,9 @@ require('./app/routing/htmlRoutes.js')(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+// app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+// });
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
